7e5be109df23d65710b37cefd87de88a55d0872a
Yoichi Imai <imai@synclogue.com>
2018-04-25 14:29:45 +0900

    Ubuntu 16.04

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -2,17 +2,19 @@
 
 set -eo pipefail
 
-if [ -z "$5" ] ; then
-  echo "$0" owner repo version asset-name path
-  echo "Be sure to set a GITHUB_TOKEN variable"
-  exit 0
+if [ -z "$4" ] ; then
+  echo "$0" owner repo version path...
+  exit 1
 fi
 
-OWNER="$1"
-REPO="$2"
-VERSION="$3"
-ASSET_NAME="$4"
-UPLOAD_FILE="$5"
+if [ -z "$GITHUB_TOKEN" ] ; then
+  echo GITHUB_TOKEN is not set
+  exit 1
+fi
+
+OWNER="$1" ; shift
+REPO="$1" ; shift
+VERSION="$1" ; shift
 API_JSON="
 {
     \"tag_name\": \"${VERSION}\",
@@ -22,7 +24,7 @@ API_JSON="
 }"
 
 RELEASE_RESPONSE=$(
-    curl -s --fail -X POST https://api.github.com/repos/${OWNER}/${REPO}/releases \
+    curl --fail -X POST https://api.github.com/repos/${OWNER}/${REPO}/releases \
         -H "Accept: application/vnd.github.v3+json" \
         -H "Authorization: token ${GITHUB_TOKEN}" \
         -H "Content-Type: application/json" \
@@ -30,10 +32,14 @@ RELEASE_RESPONSE=$(
 
 RELEASE_ID=`echo ${RELEASE_RESPONSE} | python -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
 
-CONTENT_TYPE=$(file --mime-type -b "$UPLOAD_FILE")
+for UPLOAD_FILE in "$@"; do
+  BASENAME=$(basename "$UPLOAD_FILE")
+  CONTENT_TYPE=$(file --mime-type -b "$UPLOAD_FILE")
 
-curl --fail -X POST https://uploads.github.com/repos/${OWNER}/${REPO}/releases/${RELEASE_ID}/assets?name=${ASSET_NAME} \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
-    -H "Content-Type: ${CONTENT_TYPE}" \
-    --data-binary @"${UPLOAD_FILE}"
+  echo "Uploading $UPLOAD_FILE"
+  curl --fail -X POST https://uploads.github.com/repos/${OWNER}/${REPO}/releases/${RELEASE_ID}/assets?name=${BASENAME} \
+      -H "Accept: application/vnd.github.v3+json" \
+      -H "Authorization: token ${GITHUB_TOKEN}" \
+      -H "Content-Type: ${CONTENT_TYPE}" \
+      --data-binary @"${UPLOAD_FILE}"
+done
